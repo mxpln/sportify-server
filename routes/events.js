5c784476435a61cfd3fb9dc23c2c902102132895
Maxime Paolone <paolonemax@gmail.com>
2020-05-18 15:23:27 +0200

    Events routes

--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Events = require("../models/Event");
-const Sports = require("../models/Sport");
+const User = require("../models/User");
 
 // @desc      Get all the courses
 // @route     /api/events
@@ -23,6 +23,34 @@ router.get("/solo", (req, res, next) => {
   Events.find({ type: "individual" })
     .then((eventsDocument) => {
       res.status(200).json(eventsDocument);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
+
+// @desc      Create a solo course
+// @route     /api/events/solo/new
+// @verb      POST
+router.post("/solo/new", (req, res, next) => {
+  // const sports = Sport.find()
+  const creator = req.session.currentUser._id;
+  const individualNbrOfParticipants = [creator];
+  const type = "individual";
+  const { title, description, sportType, date } = req.body;
+  const newEvents = new Events({
+    title,
+    description,
+    individualNbrOfParticipants,
+    creator,
+    sportType,
+    type,
+    date,
+  });
+  newEvents
+    .save()
+    .then((eventsDocument) => {
+      res.status(201).json(eventsDocument);
     })
     .catch((error) => {
       res.status(500).json(error);
@@ -62,14 +90,8 @@ router.post("/multi/new", (req, res, next) => {
   // const sports = Sport.find()
   const creator = req.session.currentUser._id;
   const teamA = [creator];
-  const {
-    title,
-    description,
-    sportType,
-    type,
-    maxPlayersByTeam,
-    date,
-  } = req.body;
+  const type = "collective";
+  const { title, description, sportType, maxPlayersByTeam, date } = req.body;
   const newEvents = new Events({
     title,
     description,
@@ -150,34 +172,97 @@ router.post("/multi/:id/join", (req, res, next) => {
         });
     })
     .catch((error) => {
-      console.log(error);
       res.status(500).json(error);
+    });
+});
+
+// @desc      Join a solo course
+// @route     /api/events/solo/:id/join
+// @verb      POST
+router.post("/solo/:id/join", (req, res, next) => {
+  Events.find({ type: "individual" })
+    .then(
+      Events.findByIdAndUpdate(req.params.id, {
+        $addToSet: {
+          individualNbrOfParticipants: req.session.currentUser._id,
+        },
+      })
+        .then((dbResEvents) => {
+          User.findByIdAndUpdate(req.session.currentUser._id, {
+            $addToSet: {
+              events: req.params.id,
+            },
+          })
+            .then((dbRes) => {
+              res.status(201).json(dbResEvents);
+            })
+            .catch((dbErr) => {
+              console.log(dbErr);
+            });
+        })
+        .catch((dbErr) => {
+          console.log(dbErr);
+        })
+    )
+    .catch((err) => {
+      res.status(500).json(err);
     });
 });
 
 // @desc      Leave a collective course
 // @route     /api/events/multi/:id/leave
 // @verb      DELETE
-router.delete("/multi/:id/leave", (req, res, next) => {
-  Events.findByIdAndRemove(req.params.id)
-    .then((eventsDocument) => {
-      res.status(204).json(eventsDocument);
+router.post("/multi/:id/leave", (req, res) => {
+  Events.findByIdAndUpdate(req.params.id, {
+    $pull: {
+      teamA: req.session.currentUser._id,
+      teamB: req.session.currentUser._id,
+    },
+  })
+    .then((dbRes) => {
+      User.findByIdAndUpdate(req.session.currentUser._id, {
+        $pull: {
+          events: req.params.id,
+        },
+      })
+        .then((dbResUser) => {
+          res.status(201).json(dbRes);
+          req.session.currentUser = dbResUser;
+        })
+        .catch((dbErr) => {
+          console.log(dbErr);
+        });
     })
-    .catch((error) => {
-      res.status(500).json(error);
+    .catch((dbErr) => {
+      console.log(dbErr);
     });
 });
 
-// @desc      Leave a collective course
+// @desc      Leave a solo course
 // @route     /api/events/solo/:id/leave
 // @verb      DELETE
-router.delete("/solve/:id/leave", (req, res, next) => {
-  Events.findByIdAndRemove(req.params.id)
-    .then((eventsDocument) => {
-      res.status(204).json(eventsDocument);
+router.post("/solo/:id/leave", (req, res) => {
+  Events.findByIdAndUpdate(req.params.id, {
+    $pull: {
+      individualNbrOfParticipants: req.session.currentUser._id,
+    },
+  })
+    .then((dbRes) => {
+      User.findByIdAndUpdate(req.session.currentUser._id, {
+        $pull: {
+          events: req.params.id,
+        },
+      })
+        .then((dbResUser) => {
+          res.status(201).json(dbRes);
+          req.session.currentUser = dbResUser;
+        })
+        .catch((dbErr) => {
+          console.log(dbErr);
+        });
     })
-    .catch((error) => {
-      res.status(500).json(error);
+    .catch((dbErr) => {
+      console.log(dbErr);
     });
 });
 
@@ -185,8 +270,9 @@ router.delete("/solve/:id/leave", (req, res, next) => {
 // @route     /api/events/:id/chat
 // @verb      GET
 router.get("/:id/chat", (req, res, next) => {
-  //   const { comments } = req.body;
+  // const { comments } = req.body;
   Events.findById(req.params.id, "comments")
+    .populate("author")
     .then((eventsDocument) => {
       res.status(200).json(eventsDocument);
     })
@@ -200,7 +286,6 @@ router.get("/:id/chat", (req, res, next) => {
 // @verb      POST
 router.post("/:id/chat", (req, res, next) => {
   const author = req.session.currentUser._id;
-  console.log(req.body);
   Events.findByIdAndUpdate(req.params.id, {
     $addToSet: {
       comments: { message: req.body.message, author: author },
@@ -208,6 +293,25 @@ router.post("/:id/chat", (req, res, next) => {
   })
     .then((eventsDocument) => {
       res.status(201).json(eventsDocument);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
+
+// @desc      Delete a comment
+// @route     /api/events/:id/chat/
+// @verb      DELETE
+router.delete("/:id/chat/:commentId", (req, res, next) => {
+  // const author = req.session.currentUser._id;
+  const message = req.params.commentId;
+  Events.findByIdAndUpdate(
+    req.params.id,
+    { $pull: { comments: { _id: message } } },
+    { new: true }
+  )
+    .then((post) => {
+      res.status(201).json(post);
     })
     .catch((error) => {
       res.status(500).json(error);
